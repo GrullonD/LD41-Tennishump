9274aac3c601a693f6f71f0ddb576e35fec3708c
Matt Martucciello <kassanu@gmail.com>
2018-04-21 15:03:20 -0400

    Racket return, spin
    Racket returns the ball on collision
    Can impart spin based on the speed and direction you move mouse
    ball no longer collides with player

---
+++ Assets/Scripts/Court.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Court : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Ball") {
+            print("floor collided with ball");
+        }
+    }
+}
